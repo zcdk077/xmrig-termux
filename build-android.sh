4c452ce14c51fc6aae0a0de400be3979a6ee3bb2
zcdk077 <zackynautika@gmail.com>
2025-01-15 16:57:10 +0700

    Create build-android.sh

---
+++ build-android.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+echo "Tunggu sebentar"
+echo "Update dan install library"
+pkg update && pkg upgrade -y
+pkg install wget cmake nano
+echo "Cloning and starting compiling"
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build
+cd xmrig/build
+# cmake
+echo "Proses CMake"
+if ! cmake -DWITH_HWLOC=OFF ..; then
+    echo "Error: CMake configuration failed"
+    exit 1
+else
+    cmake -DWITH_HWLOC=OFF ..
+fi
+# compile
+echo "Compiling now"
+if ! make; then
+    echo "Error: Compiling failed"
+    exit 1
+else
+    make && echo "Done... you can use run script now"
+fi
+# remove build-android.sh
+set -e
+echo "Menghapus build"
+cd ..
+echo "Proses hapus build"
+if ! rm build-android.sh; then
+    echo "Errror: Gagal hapus buid"
+    exit 1
+else
+    rm build-android.sh && echo "Build telah dihapus"
+fi
