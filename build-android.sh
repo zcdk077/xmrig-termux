b007717e0b3e50d991b58dc8696afc1829e7d150
zcdk077 <zackynautika@gmail.com>
2025-01-15 19:25:58 +0700

    Update build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -3,9 +3,11 @@
 set -e
 echo "Tunggu sebentar"
 echo "Update dan install library"
+sleep 5
 pkg update && pkg upgrade -y
 pkg install wget cmake nano
-echo "Cloning and starting compiling"
+echo "Cloning xmrig"
+sleep 5
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build
 cd xmrig/build
@@ -25,7 +27,7 @@ if ! make; then
 else
     make && echo "Done... you can use run script now"
 fi
-cd ..
+cd
 cd xmrig
 echo "Proses clone konfigurasi"
 if ! wget https://raw.githubusercontent.com/zcdk077/xmrig-termux/master/start.sh && wget https://raw.githubusercontent.com/zcdk077/xmrig-termux/master/config.json && chmod +x config.json start.sh; then
