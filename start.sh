ce566e963d15022690c4f1888a1bf86284abd3c4
zcdk077 <zackynautika@gmail.com>
2025-01-15 18:26:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,20 +2,13 @@
 
 set -e
 # Gantikan Pool, Wallet dan Password. Lihat readme untuk informasi lebih lanjut.
-ALGO="ghostrider" #Algoritma Coin
-POOL="ghostrider.sea.mine.zpool.ca:5354" #Link pool dan port
-WALLET="DRz9CYkQDmtUZUCT3YHR4i5giwhBcAAdva" # Wallet Address
-PASSWORD="c=DGB,Donations" #Password
-THREADS="8" #CPU Threads yang akan digunakan
+#ALGO="ghostrider" #Algoritma Coin
+#POOL="ghostrider.sea.mine.zpool.ca:5354" #Link pool dan port
+#WALLET="DRz9CYkQDmtUZUCT3YHR4i5giwhBcAAdva" # Wallet Address
+#PASSWORD="c=DGB,Donations" #Password
+#THREADS="8" #CPU Threads yang akan digunakan
 
 cd xmrig/build
-
-# executable permissions
-echo "Setting executable permissions"
-if ! chmod +x xmrig; then
-    echo "Error: Failed to set executable permissions"
-    exit 1
-fi
-
+source ../config.json
 echo "Starting now"
-./xmrig -a $ALGO -o $POOL -u $WALLET --tls --cpu-threads $THREADS
+./xmrig -a $pools['algo'] -o $pools['url'] -u $pools['user'] -p $pools['pass'] --pools-keepalive $pools['keepalive']
