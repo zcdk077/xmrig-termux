df1504169d48d6308e11efdf3508b81bf1135ba6
zcdk077 <zackynautika@gmail.com>
2025-01-18 19:17:42 +0700

    Update build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -1,52 +1,70 @@
 #!/bin/bash
 
-echo "Berhasil download build"
+N=$(echo -en '\033[0m')
+RD=$(echo -en '\033[07;31m') # tulisan diberi blok
+RED=$(echo -en '\033[00;31m')
+GR=$(echo -en '\033[07;32m') # tulisan diberi blok
+GRN=$(echo -en '\033[00;32m')
+YLW=$(echo -en '\033[00;33m')
+BLUE=$(echo -en '\033[00;34m')
+MTA=$(echo -en '\033[00;35m')
+LMA=$(echo -en '\033[20;36m')
+PURP=$(echo -en '\033[00;35m')
+CYAN=$(echo -en '\033[00;36m')
+LGRAY=$(echo -en '\033[00;38m')
+LRD=$(echo -en '\033[07;31m')
+LRED=$(echo -en '\033[01;31m')
+LGR=$(echo -en '\033[01;32m')
+LYL=$(echo -en '\033[01;33m')
+LYLO=$(echo -en '\033[07;33m') # tulisan diberi blok
+LBL=$(echo -en '\033[01;34m')
+LBLU=$(echo -en '\033[07;34m')
+LMA=$(echo -en '\033[01;35m')
+LPPLE=$(echo -en '\033[01;35m')
+LCY=$(echo -en '\033[01;36m')
+LCYN=$(echo -en '\033[07;36m')
+WHT=$(echo -en '\033[01;37m')
+
+echo "${GR}Berhasil download build${N}"
 sleep 3
 
 set -e
-echo "Tunggu sebentar"
+echo "${LYLO}Tunggu sebentar${N}"
+sleep 2
+echo "${LYLO}Kita update dan install library terlebih dahulu${N}"
 sleep 3
-echo "Kita update dan install library terlebih dahulu"
-sleep 5
 pkg update && pkg upgrade -y
-pkg install wget cmake nano
-echo "Cloning xmrig"
+pkg install wget nano build-essential cmake automake libtool autoconf
+echo "${LYLO}Cloning xmrig${N}"
 sleep 5
 git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build
-cd xmrig/build
+mkdir build
+cd build
 # cmake
-echo "Proses CMake"
+echo "${LYLO}Proses CMake${N}"
 sleep 5
 if ! cmake -DWITH_HWLOC=OFF ..; then
-    echo "Error: CMake configuration failed"
+    echo "${RD}Error: CMake configuration failed${N}"
     exit 1
 else
     cmake -DWITH_HWLOC=OFF ..
 fi
 # compile
-echo "Compiling now"
+echo "${LYLO}Compiling now${N}"
 sleep 5
 if ! make; then
-    echo "Error: Compiling failed"
+    echo "${RD}Error: Compiling failed${N}"
     exit 1
 else
-    make && echo "Proses Instalasi selesai"
+    make && echo "${GR}Proses Instalasi selesai${N}"
 fi
 sleep 3
-echo "Proses clone konfigurasi"
-sleep 1
-echo "Proses clone konfigurasi ."
-sleep 1
-echo "Proses clone konfigurasi . ."
-sleep 1
-echo "Proses clone konfigurasi . . ."
+echo "${LYLO}Proses clone konfigurasi${N}"
+echo ""
+echo ""
+echo ""
 sleep 3
 cd
-cd xmrig
-wget https://raw.githubusercontent.com/zcdk077/xmrig-termux/master/start.sh
 chmod +x start.sh
-wget https://raw.githubusercontent.com/zcdk077/xmrig-termux/master/config.json
 chmod +x config.json
-echo "Berhasil cloning konfigurasi"
-cd
+echo "${GR}Berhasil cloning konfigurasi${N}"
